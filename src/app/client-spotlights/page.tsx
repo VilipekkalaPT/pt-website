ae0fceb36eb4e8bdb5128eba9c5fdc480ae856db
GiangNguyen1207 <nguyengiang.nchg@gmail.com>
2025-04-15 01:27:31 +0300

    Initiate project

---
+++ src/app/client-spotlights/page.tsx
@@ -0,0 +1,8 @@
+export default function ClientSpotlights() {
+  return (
+    <div>
+      <h1>Client Spotlights</h1>
+      <p>Spotlight on our clients success stories!</p>
+    </div>
+  );
+}
---
+++ src/app/faqs/page.tsx
@@ -0,0 +1,8 @@
+export default function FAQs() {
+  return (
+    <div>
+      <h1>FAQs</h1>
+      <p>Frequently Asked Questions</p>
+    </div>
+  );
+}
