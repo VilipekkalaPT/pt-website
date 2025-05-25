21186d79b37e44440cf96202da96e06751e1384a
GiangNguyen1207 <nguyengiang.nchg@gmail.com>
2025-05-25 23:22:41 +0300

    Remove all packages button

--- src/app/pricing/components/SinglePackageContainter.tsx
+++ src/app/pricing/components/SinglePackageContainter.tsx
@@ -1,15 +1,8 @@
-"use client";
-
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
-import Button from "app/components/Button";
 import {
   TypeCurriculumFields,
   TypePackageFields,
 } from "app/lib/types/contentful";
-import { ROUTES } from "app/utils/routes";
-
 import { AssetFields } from "contentful";
-import { useRouter } from "next/navigation";
 import PackageDetails from "./PackageDetails";
 import Divider from "app/components/Divider";
 import Curriculum from "./Curriculum";
@@ -25,17 +18,8 @@ export default function SinglePackageContainer({
   curriculum,
   image,
 }: SinglePackageContainerProps) {
-  const router = useRouter();
-
   return (
     <div className="mt-30">
-      <Button
-        label={"All packages"}
-        variant="ghost"
-        iconLeft={<ArrowLeftIcon className="size-4" />}
-        onClick={() => router.push(ROUTES.PRICING)}
-        className="mx-3 mb-3"
-      />
       <PackageDetails packageDetails={packageDetails} image={image} />
       <Divider />
       <Curriculum curriculum={curriculum} />
